01ea01b52ee402296c7f51f7268a8c05a2390946
Wendel Santos <henrique.santos1988@gmail.com>
2018-08-24 22:31:41 -0300

    Botão TouchableOpacity

--- fdd/index.android.js
+++ fdd/index.android.js
@@ -1,32 +1,45 @@
 import React from 'react';
-import { AppRegistry, Text, View , Button } from 'react-native';
+import { AppRegistry, Text, View , Button , TouchableOpacity } from 'react-native';
 
 //Formatações 
 const Estilos = {
   principal: {
     paddingTop: 40
+  },
+  botao:{
+    backgroundColor: '#48BBEC',
+    padding: 10,
+    borderColor: '#1d8eb8',
+    borderWidth: 1,
+    borderRadius: 8,
+    //shadowColor: '#000',
+    //shadowOffSet: {width: 0, height: 2},
+    //shadowOpacity: 0.4
+  }, 
+  textoBotao: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    fontWeight: 'bold',
+    alignSelf: 'center'
   }
 };
 
 //
 const botaoPressionado = ()=> {
   alert('Botão Pressionado');
-}
+};
 
 //Cria o Componente.
 const app2 = () => {
   
-  const { principal } = Estilos; 
+  const { principal , botao , textoBotao } = Estilos; 
 
   return(
 
     <View style={ principal }>
-      <Button
-        onPress={botaoPressionado}
-        title="Click Aqui"
-        color="#841584"
-        accessibilityLabel="Click aqui para ler notícias!!!"
-      />
+      <TouchableOpacity style={ botao }>
+        <Text style={ textoBotao }>Clique Aqui</Text>
+      </TouchableOpacity>
     </View>
   );
 };
