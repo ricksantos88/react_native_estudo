1807e881c9295afcaa59bcbb8e44f2999c2ea5d1
Wendel Santos <henrique.santos1988@gmail.com>
2018-08-25 10:31:00 -0300

    Commit App FDD Funcional

--- fdd/index.android.js
+++ fdd/index.android.js
@@ -1,35 +1,73 @@
 import React from 'react';
-import { AppRegistry, Text, View , Button , Image } from 'react-native';
+import { AppRegistry, Text, View , TouchableOpacity , Image , Alert } from 'react-native';
 
 //Formatações 
 const Estilos = {
+
   principal: {
-    padding: 40
+    flex: 1,
+    justifyContent: 'center',
+    alignItems:'center'
   },
-  imagem:{
-    justifyContent:'flex-end',
-    padding: 5
+
+  botao:{
+    backgroundColor: '#538530',
+    paddingVertical: 10,
+    paddingHorizontal:40,
+    marginTop: 20
   },
-  textoImagem:{
-    backgroundColor:'#FFFFFF'
+
+  textoBotao:{
+    color: '#FFF',
+    fontSize: 16,
+    fontWeight: 'bold'
   }
+
 };
 
 //
+const gerarNovaFrase = () =>{
+  var numeroAleatorio = Math.random();
+  numeroAleatorio =  Math.floor(numeroAleatorio * 13);
+
+  //Frases
+  var frases = Array()
+  frases[0] = 'A esperança é como o sol. Se você apenas acredita quando vê, você nunca vai sobreviver à noite.';
+  frases[1] = 'A Força não pertence aos Jedi. Dizer que um Jedi morre, a Força morre, é vaidade.';
+  frases[2] = 'É assim que vamos ganhar. Não lutando contra o que odiamos, mas salvando o que amamos.';
+  frases[3] = 'Transmita o que aprendeu. Força, mestria. Mas fraqueza, insensatez, fracasso também. Sim, fracasso acima de tudo. O maior professor, o fracasso é. Luke, nós somos o que eles crescem além. Esse é o verdadeiro fardo de todos os mestres.';
+  frases[4] = 'Faça. Ou não faça. Não existe a tentativa.';
+  frases[5] = 'Faça. Ou não faça. Não existe a tentativa.';
+  frases[6] = 'Verdadeiramente maravilhosa, a mente de uma criança é.';
+  frases[7] = 'Guerras não faz grande ninguém.';
+  frases[8] = 'O medo é o caminho para o lado negro. O medo leva a raiva, a raiva leva ao ódio, o ódio leva ao sofrimento.';
+  frases[9] = 'Sempre passar o que você aprendeu.';
+  frases[10] = 'Seres luminosos somos nós. Não esta matéria bruta.';
+  frases[11] = 'Que a Força esteja com você.';
+  frases[12] = 'Lembre-se: seu foco determina a sua realidade.';
+
+  var fraseEscolhida = frases[ numeroAleatorio ];
+
+  alert(fraseEscolhida);
+
+}
 
 //Cria o Componente.
 const app2 = () => {
   
-  const { principal , imagem , textoImagem } = Estilos; 
+  const { principal , botao , textoBotao } = Estilos; 
 
   return(
 
     <View style={ principal }>
-      <Image style={ imagem } source={ require('./imgs/uvas.png') }>
-        <Text style={ textoImagem }>Vicio é foda</Text>
-      </Image>
+      <Image source={ require('./imgs/logo.png') }/>
+      <TouchableOpacity 
+          onPress={gerarNovaFrase}
+          style={ botao }>
+        <Text style={ textoBotao }>Frases Yoda</Text>
+      </TouchableOpacity>
     </View>
   );
 };
-
+//Renderiza para o dispositivo
 AppRegistry.registerComponent('fdd', () => app2);
